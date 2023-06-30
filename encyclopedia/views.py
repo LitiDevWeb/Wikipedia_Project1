92a0a17e29d873e0053528fd70b8d14532ad0956
LitiDevWeb <laetitiahadjout7@gmail.com>
2023-06-30 20:43:53 +0100

    Wiki Done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+import random
 
 from . import util
 
@@ -48,7 +49,57 @@ def search(request):
             return render(request, "encyclopedia/search.html", { 
                 "recommendation" : recommendation
             } )
+        
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        titleExist = util.get_entry(title)
+        if titleExist is not None:
+            return render(request, "encyclopedia/error.html",{ 
+                "message": "Entry page already exist"
+            })
+        else:
+            util.save_entry(title, content)
+            html_content = convert_md_to_html
+            return render(request,"encyclopedia/entry.html", { 
+                "title" : title,
+                "content": html_content
+            })
+        
+def edit(request):
+    if request.method == 'POST':
+        title = request.POST["entry_title"]
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/edit.html", { 
+            "title" : title,
+            "content" : content
+        })
+    
 
+def save_edit(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title, content)
+        html_content = convert_md_to_html
+        return render(request,"encyclopedia/entry.html", { 
+            "title" : title,
+            "content": html_content
+        })
+
+def rand(request):
+    allEntries = util.list_entries()
+    rand_entry = random.choice(allEntries)
+    html_content = convert_md_to_html(rand_entry)
+    return render(request, "encyclopedia/entry.html", { 
+        "title" : rand_entry,
+        "content" : html_content
+
+    })
+ 
 
 
 
